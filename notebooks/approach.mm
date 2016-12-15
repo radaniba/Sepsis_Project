e98b702a7c360ff7b7086073c9a728cb3341e597
radaniba <aradwen@gmail.com>
2016-12-15 04:55:26 -0800

    added new notebooks

---
+++ notebooks/approach.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1481650739311" ID="ID_571510561" MODIFIED="1481650739311" TEXT="New Mindmap"/>
+</map>
